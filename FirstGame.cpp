25c424cc24ebc6520a217cdc5b769f2498779abc
Juraj Chobot <36934437+Elephantiago@users.noreply.github.com>
2024-05-03 23:42:11 +0200

    Update FirstGame.cpp
    Fixing a typo in comment.

--- FirstGame.cpp
+++ FirstGame.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 using namespace literals::chrono_literals;
 
-/* There was complication with generating multiple random numbers in one run as every run there was the same number because the time was still the same when the second seed was required for srand function. Ideally you would solve it with sleep function, but C++ requires different sleep functions for Windows, Mac and Linux and as such the code would break on any other platform. My solution was adding chrono and thread libraries to simulate sleep the function with minimum required sleep time of 1000ms as anything below that can result in repeating numbers.
+/* There was complication with generating multiple random numbers in one run as every run there was the same number because the time was still the same when the second seed was required for srand function. Ideally you would solve it with sleep function, but C++ requires different sleep functions for Windows, Mac and Linux and as such the code would break on any other platform. My solution was adding chrono and thread libraries to simulate the sleep function with minimum required sleep time of 1000ms as anything below that can result in repeating numbers.
 
  It's probably not an ideal solution, but it sets the ground for the basis of the game. This is the first commit. Second one will come when I wake up.*/
 int main()
